58a64bfad4a55b26c4e28630b012445ca65b449d
Annie Deshaies <annie.deshaies@gmail.com>
2023-03-02 18:52:10 -0500

    chore: clean up of useless files

---
+++ src/styles/fontStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+const FontStyles = createGlobalStyle`
+  @font-face {
+  }
+`
+
+export default FontStyles
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+// const rem = (pixels) => {
+// 	const base16 = 16
+// 	return `${pixels / base16}rem`
+// }
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Open Sans';
+    font-weight: 400;
+    font-size: 12px;
+    position: relative;
+    overflow: hidden;
+    height: 100%;
+    color: black;
+    background-color: white;
+    opacity: 1;
+    transition: opacity 100ms linear;
+    font-variant-numeric: tabular-nums;
+  }
+`
+
+export default GlobalStyles
